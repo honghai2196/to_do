a2bd404af3693140be49d59616c19c4f2034f210
honghai2196 <honghai2196@outlook.com>
2021-07-30 02:12:10 +0700

    update locator

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,5 +1,12 @@
 import 'package:get_it/get_it.dart';
+import 'package:to_do/view/all/all_task_viewmodel.dart';
+import 'package:to_do/view/complete/complete_task_viewmodel.dart';
+import 'package:to_do/view/incomplete/incomplete_task_viewmodel.dart';
 
 GetIt locator = GetIt.instance;
 
-void setUpLocator() {}
+void setUpLocator() {
+  locator.registerFactory(() => AllTaskViewModel());
+  locator.registerFactory(() => CompleteTaskViewModel());
+  locator.registerFactory(() => IncompleteTaskViewModel());
+}
